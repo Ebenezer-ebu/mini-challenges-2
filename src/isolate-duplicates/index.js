0d98d38ca50438585ffc5e66c5d9a50a0df3ff53
ebenezer ebu <eifezulike@gmail.com>
2020-11-12 01:06:50 +0100

    In Progress

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,15 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
+    let tempArr = text.split('');
+    let letter = [];
+    let string = ``;
+    for(let i = 0; i < tempArr.length; i++) {
+      if(tempArr[i] === tempArr[i+1]) {
+        letter.push(tempArr[i]); 
+        letter.push(tempArr[i+1])
+      }
+    }  
+    return letter;
+  }
+  (isolateDuplicates('eeaiij'));
 
 module.exports = isolateDuplicates;
