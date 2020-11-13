bdaac8761938e25b1cd77cce38ff11dc3406a919
ebenezer ebu <eifezulike@gmail.com>
2020-11-13 19:25:14 +0100

    Good

--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,33 +1,29 @@
 function removeDuplicates(obj) {
-  let myObj = obj;
-  let unique;
-  let keys = Object.keys(myObj);
-  console.log(keys)
-  let values = Object.values(myObj);
-  let pos;
-  let newObj = new Map();
-  for(let i = 0; i < keys.length; i++) {
-     pos = keys[i];
-     unique = [...new Set(myObj[pos])];
-     newObj.set(pos,unique);
-     console.log(newObj.entries);
+    Object.keys(obj).sort();
+    let object;
+    let newObj = {};
+    for (let prop in obj) {
+       newObj[prop] = [...new Set(obj[prop])];     
+    }
+    let val = Object.values(newObj);
+    console.log(val)
+    for(let i = 0; i < val.length - 1; i++) {
+      let array1 = val[i];
+      let array2 = val[i+1];
+      array1 = array1.filter(function(item) {
+      return !array2.includes(item);
+      });
+      
+      let keyItem = Object.keys(newObj);
+      newObj[keyItem[i]] = array1;  
+    }
+    return newObj;
   }
-  console.log(newObj.entries);
-  let newKeys = Object.keys(newObj);
-  console.log(newKeys);
-  let newValues = Object.values(newObj);
-  console.log(newValues);
-  newKeys.sort(((a, b) => a - b));
-  for(let j = 0; j < newValues.length; j++) {
-    let srt = newValues[j];
-    
-  }
-
-}
-
-console.log(removeDuplicates({
-  "1": ["A", "B", "C"],
-  "2": ["A", "B", "D", "A"]
-}));
+  
+  console.log(removeDuplicates({
+    "1": ["A", "B", "C"],
+    "2": ["A", "B", "D", "A"],
+    "3": ["F", "B", "C", "C", "G"]
+  }));
 
 module.exports = removeDuplicates;
