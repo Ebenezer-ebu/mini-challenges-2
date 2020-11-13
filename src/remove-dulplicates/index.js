da1983c04e4e5f2dd22c552e228b3001d6ddb28d
ebenezer ebu <eifezulike@gmail.com>
2020-11-13 15:20:08 +0100

    remove in progress

--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,33 @@
-function removeDuplicates(obj) {}
+function removeDuplicates(obj) {
+  let myObj = obj;
+  let unique;
+  let keys = Object.keys(myObj);
+  console.log(keys)
+  let values = Object.values(myObj);
+  let pos;
+  let newObj = new Map();
+  for(let i = 0; i < keys.length; i++) {
+     pos = keys[i];
+     unique = [...new Set(myObj[pos])];
+     newObj.set(pos,unique);
+     console.log(newObj.entries);
+  }
+  console.log(newObj.entries);
+  let newKeys = Object.keys(newObj);
+  console.log(newKeys);
+  let newValues = Object.values(newObj);
+  console.log(newValues);
+  newKeys.sort(((a, b) => a - b));
+  for(let j = 0; j < newValues.length; j++) {
+    let srt = newValues[j];
+    
+  }
+
+}
+
+console.log(removeDuplicates({
+  "1": ["A", "B", "C"],
+  "2": ["A", "B", "D", "A"]
+}));
 
 module.exports = removeDuplicates;
