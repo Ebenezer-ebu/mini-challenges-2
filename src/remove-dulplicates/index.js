b6449c5cecf4b04e6e5290bbd99bb6c5c7ac4a51
ebenezer ebu <eifezulike@gmail.com>
2020-11-17 13:06:55 +0100

    Good

--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -21,11 +21,6 @@ function removeDuplicates(obj) {
     return newObj;
   }
   
-  console.log(removeDuplicates({
-   "432": ["A", "A", "B", "D"],
-    "53": ["L", "G", "B", "C"],
-    "236": ["L", "A", "X", "G", "H", "X"],
-    "11": ["P", "R", "S", "D"]
-  }));
+  
 
 module.exports = removeDuplicates;
